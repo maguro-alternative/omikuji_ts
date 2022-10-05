0a6c60bb0985decbb3a06cb43fe55029b96c28d7
ygo <ka.ra.si.naporitan@gmail.com>
2022-10-05 18:56:28 +0900

    master

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [question,setText] = React.useState('?');
+
+    React.useEffect(()=>{
+      if(question === '?'){
+        return
+      };
+      onClick();
+    },[question]);
+
+
+    const onClick = () => {
+      const point = Math.random();
+      if(point <= 0.01 && point >= 0.0) {
+        setText('当たり！！'+point);
+      }
+      /*else if(point >= 0.31 && point <= 0.5) {
+        setText('中吉');
+      }*/
+      else if(point >= 0.02 && point <= 0.99){
+        setText('はずれ、、'+point);
+      }
+      console.log("結果")
+      console.log(point)
+    }
+
+    return(
+      <div className="wrap">
+        <h2>おみくじ</h2>
+        <div id="btn" onClick={onClick}>{question}</div>
+      </div>
+    );
 }
 
 export default App;
