40bb8a57c9bf0653db47e3290f0720b32902d2ff
ygo <ka.ra.si.naporitan@gmail.com>
2022-10-05 19:05:38 +0900

    master

--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,13 @@ function App() {
 
     const onClick = () => {
       const point = Math.random();
-      if(point <= 0.01 && point >= 0.0) {
+      if(point <= 0.1 && point >= 0.0) {
         setText('当たり！！'+point);
       }
       /*else if(point >= 0.31 && point <= 0.5) {
         setText('中吉');
       }*/
-      else if(point >= 0.02 && point <= 0.99){
+      else if(point >= 0.11 && point <= 0.99){
         setText('はずれ、、'+point);
       }
       console.log("結果")
